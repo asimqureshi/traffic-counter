dfaa65128540ff02ab1db81ef4645fbab30e250c
m-asim-qureshi <asim.qureshi@aurea.com>
2021-11-30 18:18:36 +1100

    fix for when some of the line items in payload body are not parseable

--- src/main/java/test/adnuntius/trafficcounter/transformer/VehiclePassedEventTransformer.java
+++ src/main/java/test/adnuntius/trafficcounter/transformer/VehiclePassedEventTransformer.java
@@ -7,16 +7,33 @@ import test.adnuntius.trafficcounter.model.VehicleSize;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
 public class VehiclePassedEventTransformer {
 
     public List<VehiclePassedEvent> apply(String request) {
-        return Arrays.stream(request.split(",")).filter(eventString -> !eventString.isEmpty()).map(eventString -> {
-            String[] attributes = Arrays.stream(eventString.split("_")).map(String::trim).collect(Collectors.toList()).toArray(new String[0]);
-            return new VehiclePassedEvent(attributes[0], LocalDateTime.parse(attributes[1]), VehicleSize.valueOf(attributes[2]), Integer.parseInt(attributes[3]));
-        }).collect(Collectors.toList());
+        return Arrays.stream(request.split(","))
+                .filter(eventString -> !eventString.isEmpty())
+                .map(eventString -> {
+                    List<String> attributes = Arrays.stream(eventString.split("_"))
+                            .map(String::trim).collect(Collectors.toList());
+                    return newVehiclePassedEvent(attributes);
+                }).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    private VehiclePassedEvent newVehiclePassedEvent(List<String> attributes){
+
+        try {
+            return new VehiclePassedEvent(attributes.get(0), LocalDateTime.parse(attributes.get(1)),
+                    VehicleSize.valueOf(attributes.get(2)), Integer.parseInt(attributes.get(3)));
+        } catch (Exception e) {
+            //ignore
+            System.err.println(e.getMessage());
+        }
+
+        return null;
     }
 
 }
--- src/test/java/test/adnuntius/trafficcounter/transformer/VehiclePassedEventTransformerTest.java
+++ src/test/java/test/adnuntius/trafficcounter/transformer/VehiclePassedEventTransformerTest.java
@@ -2,6 +2,7 @@ package test.adnuntius.trafficcounter.transformer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import test.adnuntius.trafficcounter.model.VehiclePassedEvent;
 import test.adnuntius.trafficcounter.model.VehicleSize;
 
@@ -12,19 +13,33 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@SpringBootTest
 class VehiclePassedEventTransformerTest {
 
     @Autowired
     VehiclePassedEventTransformer transformer;
 
     @Test
-    void get() {
+    void testApply() {
         String request = """
                 18673541_2020-10-07T07:34:06_M_058,
                 18673541_2020-12-01T05:18:06_S_061,
                 18896541_2020-10-07T07:36:06_XL_056""";
         List<VehiclePassedEvent> expected = Arrays.asList(new VehiclePassedEvent("18673541", LocalDateTime.of(2020, 10, 07, 07, 34, 06), VehicleSize.M, 58),
                 new VehiclePassedEvent("18673541", LocalDateTime.of(2020, 12, 01, 05, 18, 06), VehicleSize.S, 61),
+                new VehiclePassedEvent("18896541", LocalDateTime.of(2020, 10, 07, 07, 36, 06), VehicleSize.XL, 56));
+
+        List<VehiclePassedEvent> events = transformer.apply(request);
+        assertThat(events, is(expected));
+    }
+
+    @Test
+    void testApply_whenOneOfTheLineItemsIsFaulty() {
+        String request = """
+                18673541_2020-10-07T07:34:06_M_058,
+                FAULTY_LINE_ITEM,
+                18896541_2020-10-07T07:36:06_XL_056""";
+        List<VehiclePassedEvent> expected = Arrays.asList(new VehiclePassedEvent("18673541", LocalDateTime.of(2020, 10, 07, 07, 34, 06), VehicleSize.M, 58),
                 new VehiclePassedEvent("18896541", LocalDateTime.of(2020, 10, 07, 07, 36, 06), VehicleSize.XL, 56));
 
         List<VehiclePassedEvent> events = transformer.apply(request);
